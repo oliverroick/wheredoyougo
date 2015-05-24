7cd0acd6b9d64d0567dcb84babea181273e4a41d
Oliver Roick <oliver.roick@gmail.com>
2015-05-24 19:59:57 +0100

    Design fixes

--- js/views/map.js
+++ js/views/map.js
@@ -1,6 +1,13 @@
 (function (global) {
     'use strict';
 
+    var STYLE = {
+        stroke: false,
+        fill: true,
+        fillColor: '#22313F',
+        fillOpacity: 0.4
+    };
+
     var Map = function () {
         L.mapbox.accessToken = SETTINGS.MAPBOX.pk;
         this.map = L.mapbox.map('map', 'mapbox.streets-basic', {zoomControl: false}).setView([51.50719323477933, -0.12754440307617188], 9);
@@ -10,12 +17,8 @@
         var markers = [];
         for (var key in checkins) {
             var checkin = checkins[key];
-            var marker = L.circleMarker(checkin.location, {
-                stroke: false,
-                fill: true,
-                fillColor: '#22313F'
-            });
-            var radius = checkin.checkins <= 41 ? 9 + checkin.checkins : 50;
+            var marker = L.circleMarker(checkin.location, STYLE);
+            var radius = checkin.checkins <= 41 ? 10 + Math.floor(checkin.checkins/2) : 25;
             marker.setRadius(radius)
             marker.addTo(this.map);
         }
